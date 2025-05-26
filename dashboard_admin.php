79cb038de21cff43cd1ccccfa7dfd20f831af59e
TheoCollin54 <theo.collin054@gmail.com>
2025-05-26 10:20:17 +0200

    Update dashboard_admin.php

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -78,6 +78,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard administrateur</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php foreach ($users as $user): ?>
